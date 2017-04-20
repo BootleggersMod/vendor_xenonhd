a803d69be74ca674694d40f56a030a297cc440eb
dadi11 <dadi.sikorski@gmail.com>
2017-04-20 20:11:39 +0200

    xenonhd: change XenonOTA app

--- config/ota.mk
+++ config/ota.mk
@@ -9,20 +9,13 @@ DEVICE := $(subst xenonhd_,,$(TARGET_PRODUCT))
 
 # XenonHD OTA app
 PRODUCT_PACKAGES += \
-    XenonOTA
+    XenonOTA \
+    libbypass
 
 # Build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xenonhd.version=$(XENONHD_VERSION) \
     ro.ota.device=$(DEVICE) \
-    ro.ota.type=$(OTA_TYPE)
-
-$(shell echo -e "# OTA_configuration\n \
-ota_url=https://mirrors.c0urier.net/android/teamhorizon/N/OTA/ota_nougat_$(DEVICE).xml\n \
-ota-device=$(DEVICE)\n \
-device_name=ro.ota.device\n \
-release_type=Stable\n \
-version_source=ro.xenonhd.version\n \
-version_delimiter=-\n \
-version_position=1\n \
-version_format=yyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)
+    ro.ota.type=$(OTA_TYPE) \
+    ro.ota.version=$(shell date +"%y%m%d") \
+    ro.ota.manifest=https://mirrors.c0urier.net/android/teamhorizon/N/OTA/ota_nougat_$(DEVICE).xml
